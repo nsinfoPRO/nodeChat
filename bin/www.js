2c7708875fbeba225dcffe3fcecf5ee731ec9965
Nenad <nenadspasenic@live.com>
2018-10-20 15:46:00 +0200

    verzija6

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,9 @@
 var app=require('express')();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
-var port=process.env.OPENSHIFT_NODEJS_PORT || 5000;   //ako heroku ne dodeli port onda uzmi 5000 kao port
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-app.set('port',port);
+var port=process.env.OPENSHIFT_NODEJS_PORT || 3000;   //ako heroku ne dodeli port onda uzmi 5000 kao port
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get('/',function (req,res) {
     res.sendFile(__dirname+'\\index2.html');
     
